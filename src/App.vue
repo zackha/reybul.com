595c5cde3857828712f71d161661fd245546f7b8
Zack <zckhtln@icloud.com>
2022-04-26 08:30:19 +0300

    main

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+  <AppHeader />
+
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    AppHeader
   }
 }
+
 </script>
 
 <style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,229 @@
+<template>
+    <!-- #header -->
+      <header>
+        <div class="component container main-navigation-container">
+          <div class="component-content">
+            <div class="component link skip-content">
+              <div class="component-content">
+                <div class="field-link">
+                  <a href="#body" data-variantitemid="{7D7960AE-1EAE-408A-9DBB-99B3F5FAB525}" data-variantfieldname="Link" class="btn btn--filled">Skip to the main content</a>
+                </div>
+              </div>
+            </div>
+            <div class="component content">
+              <div class="component-content">
+                <div class="navbar js-navbar" data-nav-label-primary="Primary" data-nav-label-secondary="Subsections">
+                  <div class="navbar__main-bar">
+                    <div class="navbar__main-bar-inner">
+                      <div class="component image file-type-icon-media-link">
+                        <div class="component-content">
+                          <a title="Home" href="en.html" class="navbar__brand">
+                            <img src="images/brand-default.svg?h=36&amp;iar=0&amp;w=88&amp;rev=23ee61ddbb1948f399b47938edeb3c11&amp;hash=2B23017C5CF442F0053CB73C35010BD6" alt="Home" width="88" height="36" data-variantitemid="{6B4624D9-0FD5-4869-B151-680FEC414EBA}" data-variantfieldname="Image">
+                          </a>
+                          <span class="image-caption field-imagecaption"></span>
+                        </div>
+                      </div>
+                      <div class="component navigation navigation-title nav-desktop-sections js-navbar-sections main-nav-desktop nav-desk-expand-serach-box">
+                        <div class="component-content">
+                          <nav>
+                            <ul class="clearfix">
+                              <li class=" level1 submenu item0 odd first rel-level1">
+                                <a title="About us" href="en/about-us.html">About us</a>
+                              </li>
+                              <li class=" level1 submenu item1 even rel-level1">
+                                <a title="Life at home" href="en/life-at-home.html">Transparency</a>
+                              </li>
+                              <li class=" level1 submenu item3 even rel-level1">
+                                <a title="Sustainability | About IKEA" href="en/sustainability.html">Sustainability</a>
+                              </li>
+                            </ul>
+                          </nav>
+                        </div>
+                      </div>
+                      <div class="component link link-with-icon hidden-until-xl desktop-header-ex-link expand-search-show">
+                        <div class="component-content">
+                          <a class="link--black" target="_blank" title="" href="http://www.ikea.com"> Work with us </a>
+                        </div>
+                      </div>
+                      <div class="navbar__mobile-region js-navbar-focus-trap">
+                        <input class="navbar__checkbox" type="checkbox" id="nav-toggle" aria-label="navigation-toggle">
+                        <button class="navbar__button js-navbar-toggle-menu navbar__toggle-menu" aria-controls="sections-navigation" aria-expanded="false" title="Open navigation" aria-label="Toggle navigation">
+                          <svg class="svg-icon svg-icon--black svg-icon--150 navbar__icon-menu" aria-hidden="true">
+                            <use xlink:href="/-/media/Themes/NL/AboutIKEA/AboutIKEA/svg/symbol/svg-sprite.svg#menu"></use>
+                          </svg>
+                          <svg class="svg-icon svg-icon--black svg-icon--150 navbar__icon-close" aria-hidden="true">
+                            <use xlink:href="/-/media/Themes/NL/AboutIKEA/AboutIKEA/svg/symbol/svg-sprite.svg#close"></use>
+                          </svg>
+                        </button>
+                        <noscript>
+                          <label class="navbar__button navbar__toggle-menu" for="nav-toggle" tabindex="1">
+                            <svg class="svg-icon svg-icon--black svg-icon--150 navbar__icon-menu">
+                              <use xlink:href="/-/media/Themes/NL/AboutIKEA/AboutIKEA/svg/symbol/svg-sprite.svg#menu"></use>
+                            </svg>
+                            <svg class="svg-icon svg-icon--black svg-icon--150 navbar__icon-close">
+                              <use xlink:href="/-/media/Themes/NL/AboutIKEA/AboutIKEA/svg/symbol/svg-sprite.svg#close"></use>
+                            </svg>
+                          </label>
+                        </noscript>
+                        <div class="component navigation nav-mobile js-navbar-mobile-menu">
+                          <div class="component-content">
+                            <div class="component navigation navigation-title navigation nav-site">
+                              <div class="component-content">
+                                <nav>
+                                  <ul class="clearfix">
+                                    <li class=" level1 submenu item0 odd first">
+                                      <button class="nav-site__row-btn">
+                                        <a title="About us" href="en/about-us.html">About us</a>
+                                        <svg class="svg-icon svg-icon--150">
+                                          <use xlink:href="/-/media/Themes/NL/AboutIKEA/AboutIKEA/svg/symbol/svg-sprite.svg#more-regular"></use>
+                                        </svg>
+                                      </button>
+                                      <ul class="clearfix">
+                                        <li class=" level2 item0 odd first">
+                                          <a title="History of IKEA" href="en/about-us/history-of-ikea.html">The history of IKEA</a>
+                                        </li>
+                                        <li class=" level2 item1 even">
+                                          <a title="IKEA retail" href="en/about-us/ikea-retail.html">The IKEA way of retail</a>
+                                        </li>
+                                        <li class=" level2 item2 odd">
+                                          <a title="The IKEA franchise system" href="en/about-us/the-ikea-franchise-system.html">The IKEA franchise system</a>
+                                        </li>
+                                        <li class=" level2 item3 even">
+                                          <a title="Year in review" href="en/about-us/year-in-review.html">Year in review</a>
+                                        </li>
+                                        <li class=" level2 item4 odd">
+                                          <a title="IKEA culture and values" href="en/about-us/ikea-culture-and-values.html">IKEA culture and values</a>
+                                        </li>
+                                        <li class=" level2 item5 even last">
+                                          <a title="The IKEA value chain" href="en/about-us/the-ikea-value-chain.html">The IKEA value chain</a>
+                                        </li>
+                                      </ul>
+                                    </li>
+                                    <li class=" level1 submenu item1 even">
+                                      <button class="nav-site__row-btn">
+                                        <a title="Life at home" href="en/life-at-home.html">Life at home</a>
+                                        <svg class="svg-icon svg-icon--150">
+                                          <use xlink:href="/-/media/Themes/NL/AboutIKEA/AboutIKEA/svg/symbol/svg-sprite.svg#more-regular"></use>
+                                        </svg>
+                                      </button>
+                                      <ul class="clearfix">
+                                        <li class=" level2 item0 odd first">
+                                          <a title="Behind the scenes" href="en/life-at-home/behind-the-scenes.html">IKEA behind the scenes</a>
+                                        </li>
+                                        <li class=" level2 item1 even last">
+                                          <a title="Co-creation" href="en/life-at-home/co-creation.html">Co-creation</a>
+                                        </li>
+                                      </ul>
+                                    </li>
+                                    <li class=" level1 item2 odd">
+                                      <button class="nav-site__row-btn">
+                                        <a title="Behind the scenes" href="en/life-at-home/behind-the-scenes.html">Behind the scenes</a>
+                                        <svg class="svg-icon svg-icon--150">
+                                          <use xlink:href="/-/media/Themes/NL/AboutIKEA/AboutIKEA/svg/symbol/svg-sprite.svg#more-regular"></use>
+                                        </svg>
+                                      </button>
+                                    </li>
+                                    <li class=" level1 submenu item3 even">
+                                      <button class="nav-site__row-btn">
+                                        <a title="Sustainability | About IKEA" href="en/sustainability.html">Sustainability</a>
+                                        <svg class="svg-icon svg-icon--150">
+                                          <use xlink:href="/-/media/Themes/NL/AboutIKEA/AboutIKEA/svg/symbol/svg-sprite.svg#more-regular"></use>
+                                        </svg>
+                                      </button>
+                                      <ul class="clearfix">
+                                        <li class=" level2 item0 odd first">
+                                          <a title="All" href="en/sustainability.html">View all</a>
+                                        </li>
+                                        <li class=" level2 item1 even">
+                                          <a title="Healthy and sustainable living | About IKEA" href="en/sustainability/healthy-and-sustainable-living.html">Healthy and sustainable living</a>
+                                        </li>
+                                        <li class=" level2 item2 odd">
+                                          <a title="Stakeholder engagement" href="en/sustainability/stakeholder-engagement.html">Stakeholder engagement</a>
+                                        </li>
+                                        <li class=" level2 item3 even">
+                                          <a title="Sustainability governance" href="en/sustainability/sustainability-governance.html">Sustainability governance</a>
+                                        </li>
+                                        <li class=" level2 item4 odd">
+                                          <a title="Sustainability report highlights" href="en/sustainability/sustainability-report-highlights.html">Highlights from the IKEA Sustainability Report</a>
+                                        </li>
+                                        <li class=" level2 item5 even">
+                                          <a title="A world without waste | About IKEA" href="en/sustainability/a-world-without-waste.html">Transforming into a circular business</a>
+                                        </li>
+                                        <li class=" level2 item6 odd">
+                                          <a title="Becoming climate positive | About IKEA" href="en/sustainability/becoming-climate-positive.html">Becoming climate positive</a>
+                                        </li>
+                                        <li class=" level2 item7 even">
+                                          <a title="Responsible sourcing | About IKEA" href="en/sustainability/responsible-sourcing.html">Responsible sourcing</a>
+                                        </li>
+                                        <li class=" level2 item8 odd">
+                                          <a title="Fair and equal | About IKEA" href="en/sustainability/fair-and-equal.html">Fair and equal</a>
+                                        </li>
+                                        <li class=" level2 item9 even last">
+                                          <a title="Building a better business with IWAY" href="en/sustainability/building-a-better-business-with-iway.html">A look at our code of conduct, IWAY</a>
+                                        </li>
+                                      </ul>
+                                    </li>
+                                    <li class=" level1 submenu item4 odd">
+                                      <button class="nav-site__row-btn">
+                                        <a title="Work with IKEA" href="en/work-with-us.html">Work with us</a>
+                                        <svg class="svg-icon svg-icon--150">
+                                          <use xlink:href="/-/media/Themes/NL/AboutIKEA/AboutIKEA/svg/symbol/svg-sprite.svg#more-regular"></use>
+                                        </svg>
+                                      </button>
+                                      <ul class="clearfix">
+                                        <li class=" level2 item0 odd first">
+                                          <a title="Work at IKEA" href="https://about.ikea.com/en/work-with-us/work-at-ikea">Work at IKEA</a>
+                                        </li>
+                                        <li class=" level2 item1 even">
+                                          <a title="What is it like to be an IKEA franchisee" href="en/work-with-us/what-is-it-like-to-be-an-ikea-franchisee.html">Being an IKEA franchisee</a>
+                                        </li>
+                                        <li class=" level2 item2 odd last">
+                                          <a title="For suppliers" href="en/work-with-us/for-suppliers.html">For suppliers</a>
+                                        </li>
+                                      </ul>
+                                    </li>
+                                    <li class=" level1 submenu item5 even last">
+                                      <button class="nav-site__row-btn">
+                                        <a title="Newsroom" href="en/newsroom.html">Newsroom</a>
+                                        <svg class="svg-icon svg-icon--150">
+                                          <use xlink:href="/-/media/Themes/NL/AboutIKEA/AboutIKEA/svg/symbol/svg-sprite.svg#more-regular"></use>
+                                        </svg>
+                                      </button>
+                                      <ul class="clearfix">
+                                        <li class=" level2 item0 odd first last">
+                                          <a title="Spokespersons" href="en/newsroom/spokespersons.html">Spokespersons</a>
+                                        </li>
+                                      </ul>
+                                    </li>
+                                  </ul>
+                                </nav>
+                              </div>
+                            </div>
+                            <div class="component link link-with-icon link--black nav-site__row-btn trailing-icon mobile-header-ex-link">
+                              <div class="component-content">
+                                <a class="link--black" target="_blank" title="" href="http://www.ikea.com"> Shop at IKEA </a>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <!-- /#header -->
+</template>
+
+<script>
+export default {
+    name: "AppHeader"
+}
+</script>
+
+<style scoped>
+
+</style>
